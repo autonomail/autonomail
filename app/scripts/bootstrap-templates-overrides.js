efd4dc8648baca4df0fb51f1f4ba860505af0475
Ramesh Nair <ram@hiddentao.com>
2013-11-06 14:36:07 +0800

    Remove generated files.

--- app/scripts/bootstrap-templates-overrides.js
+++
@@ -1,7 +0,0 @@
-angular.module('App').run(['$templateCache', function($templateCache) {
-
-  $templateCache.put('template/progressbar/bar.html',
-    "<div class=\"progress-bar\" ng-class=\"type && 'progress-bar-' + type\"></div>"
-  );
-
-}]);
