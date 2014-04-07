24e45549e18de5388444e44c1a268a41b72d9a76
Ramesh Nair <ram@hiddentao.com>
2014-04-07 19:02:38 +0800

    UI for importing keys
    #25

--- app/scripts/user/controllers/keys.js
+++ app/scripts/user/controllers/keys.js
@@ -2,24 +2,101 @@
 
 (function(app) {
 
-  app.controller('KeysCtrl', function (GPG, UserMgr, AuthCredentials, $scope, Log) {
+  app.controller('KeysCtrl', function (GPG, UserMgr, AuthCredentials, $scope, $modal, Log) {
     $scope.userId = UserMgr.getCurrentUser();
     var log = Log.create('KeysCtrl(' + $scope.userId + ')');
 
     $scope.user = AuthCredentials.get($scope.userId);
 
+    /**
+     * Refresh the list of keys.
+     * @return {Promise}
+     */
+    var refreshKeys = function() {
+      log.debug('Fetching GPG keys...');
+      return GPG.getAllKeys($scope.user.email)
+        .then(function(keys) {
+          $scope.keys = keys;          
+        });
+    };
+
+
+    /**
+     * Add a PGP key.
+     */
+    $scope.addKey = function() {
+      // show a modal
+      var modalInstance = $modal.open({
+        templateUrl: 'app/modals/addKey.html',
+        controller: 'AddKeyModalCtrl',
+        keyboard: true,
+        backdrop: 'static'
+      });
+
+      // once closed
+      modalInstance.result.then(function() {
+        refreshKeys();
+      }, function(err) {
+        log.error(err.toString());
+      });
+    };
+
+
     UserMgr.ensureUserHasSecureDataSetup()
       .then(function() {
-        log.debug('Fetching GPG keys...');
-        return GPG.getAllKeys($scope.user.email);
+        return refreshKeys();        
       })
       .then(function(keys) {
-        $scope.keys = keys;
       })
       .catch(function(err) {
         log.error(err.message);
       });
   });
+
+
+
+
+  app.controller('AddKeyModalCtrl', function($scope, RuntimeError, $modalInstance, GPG, Log) {
+    var log = Log.create('AddKeyModalCtrl');
+
+    $scope.modal = $modalInstance;
+
+    // once opened
+    $modalInstance.opened.then(function() {
+      // nothing to do
+    }, function(err) {
+      $modalInstance.dismiss(new RuntimeError('Unable to open add key modal', err));
+    });
+  });
+
+
+
+  app.controller('AddKeyFormCtrl', function($scope, RuntimeError, GPG, Log) {
+    var log = Log.create('AddKeyFormCtrl');
+
+    /**
+     * Get whether form can be submitted.
+     * @return {Boolean} true if so; false otherwise.
+     */
+    $scope.canSubmit = function() {
+      return $scope.addKeyForm.$valid;
+    };  
+
+    /**
+     * Submit the form.
+     */
+    $scope.submit = function() {
+      log.debug('Import GPG key...');
+
+      GPG.importKey($scope.publicKey)
+        .then($scope.modal.close)
+        .catch(function(err) {
+          $scope.error = err.message;
+        });
+    };
+
+  });
+
 
 }(angular.module('App.user', ['App.common', 'ui.bootstrap.pagination'])));
   
