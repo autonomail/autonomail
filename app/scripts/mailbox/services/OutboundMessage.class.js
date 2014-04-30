51cd7043b80e6441e7a3e1caf5385725fb159ff0
Ramesh Nair <ram@hiddentao.com>
2014-04-30 16:59:40 +0800

    Work towards shifting public key checking into OutboundMessage

--- app/scripts/mailbox/services/OutboundMessage.class.js
+++ app/scripts/mailbox/services/OutboundMessage.class.js
@@ -2,55 +2,124 @@
   'use strict';
 
 
-  app.factory('OutboundMessage', function(MailMessage, GPG, Log) {
+  app.factory('OutboundMessage', function($q, $timeout, GPG, Log) {
 
-    var OutboundMessage = MailMessage.extend({
+    var OutboundMessage = Class.extend({
       /**
        * Constructor
        *
        * @override
        */
-      init: function(mailBox, msg) {
-        this._super(mailBox, msg);
+      init: function() {
+        this.log = Log.create('OutboundMessage');
+        this.log.debug('created');
+        
+        this._raw = {
+          to: '',
+          cc: '',
+          bcc: '',
+          subject: '',
+          body: ''
+        };
 
-        this._sendState = 'created';
+        this._missingKeys = {
+          to: [],
+          cc: [],
+          bcc: []
+        };
+
+        this._processed = {};
+
+        this._state = 'created';
       },
 
 
+
+
       /**
-       * Send this message.
+       * Get list of GPG identities for which we have public keys.
        *
-       * This will sign and encrypt the message and then send it to all 
-       * recipients.
+       * We'll cache the results so we only want it to be called once. In case 
+       * it gets called multiple times in quick succession let's use a timer 
+       * to ensure GPG only actually gets called once.
        * 
-       * @override
+       * @return {Promise}
        */
-      send: function() {
+      _getIdentitiesWithPublicKeys: function() {
         var self = this;
 
-        // basic processing
-        self.process()
-          .then(function signAndEncrypt() {
-            // for each recipient
-            _.each(self.processed.to, function(nameEmail) {
-              var emailAddress = nameEmail.email;
+        var defer = $q.defer();
 
-
-            });            
-          })  
-
-
-        if (!self._processed) {
-          self.parsed.from = _.str.extractNamesAndEmailAddresses(
-            self.raw.from
-          ).pop();
-
-          self.parsed.to = _.str.extractNamesAndEmailAddresses(
-            self.raw.to
-          );
-
-
+        if (self._gpgIdentitiesTimer) {
+          $timeout.cancel(self._gpgIdentitiesTimer);
         }
+
+        self._gpgIdentitiesTimer = $timeout(function() {
+
+          $q.when()
+            .then(function() {
+              if (!self._cachedGPGIdentities) {
+                return GPG.getAllKeys()
+                  .then(function extractIdentities(keys) {
+                    self._cachedGPGIdentities = _.chain(keys)
+                      .pluck('identities')  // [[{}]]
+                      .flatten() // [{}]
+                      .pluck('text') // []
+                      .value();
+                  });
+              }
+            })
+            .then(function done() {
+              defer.resolve(self._cachedGPGIdentities);
+            })
+            .catch(defer.reject);
+
+        }, 100, false);
+
+
+        return defer.promise;
+      },
+
+
+
+      /**
+       * Process the raw inputs.
+       *
+       * For the to, cc and bcc fields this check to see if any public keys 
+       * are missing (the `missingKey` field will get set accordingly).
+       * 
+       * @return {Promise}
+       */
+      process: function() {
+        var self = this;
+
+        self._processed = {};
+
+        return self._getIdentitiesWithPublicKeys()
+          .then(function checkPublicKeys(gpgIdentities) {
+            // for each list of addresses
+            _.each(['to', 'cc', 'bcc'], function(field){
+              self._missingKeys[field] = [];
+
+              self._processed[field] = 
+                _.str.extractNamesAndEmailAddresses(self._raw[field] || '');
+
+              // check to see if we have a public key for each address
+              _.each(self.processed[field], function(recipient) {
+                var checkStr = '<' + recipient.email + '>';
+
+                var found = _.find(gpgIdentities, function(identity) {
+                  return 0 <= identity.indexOf(checkStr);
+                });
+
+                recipient.havePublicKey = (undefined === found);
+
+                if (!recipient.havePublicKey) {
+                  self._missingKeys[field].push(recipient.email);
+                }
+              });
+            });
+          });
       },
 
 
@@ -70,15 +139,26 @@
 
     });
 
+    
+    /**
+     * Raw message inputs.
+     */
+    OutboundMessage.prop('raw', { internal: '_raw' });
 
+    /**
+     * Recipients for whom we're missing public keys.
+     */
+    OutboundMessage.prop('missingKeys', { internal: '_missingKeys' });
 
-    OutboundMessage.prop('sendState', {
-      set: false,
-      get: function() {
-        return this._sendState;
-      }
-    });
+    /**
+     * Processed inputs, i.e. emails parsed, encrypted, signed, etc.
+     */
+    OutboundMessage.prop('processed', { internal: '_processed' });
 
+    /**
+     * Current message state.
+     */
+    OutboundMessage.prop('state', { internal: '_state' });
 
 
     return OutboundMessage;
